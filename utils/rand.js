19945e1981a484cb3cdd72f033c7afc43cbdf7b7
sarmerer <sarmerer@gmail.com>
2024-06-29 04:04:50 +0500

    feat - encapsulate utils

--- utils/rand.js
+++ utils/rand.js
@@ -1,18 +1,20 @@
-window.smauc = window.smauc = window.smauc || {}
+;(function () {
+  window.smauc = window.smauc = window.smauc || {}
 
-window.smauc.rand = {
-  gaussianDistribution(mean, stddev) {
-    let u = 0
-    let v = 0
+  window.smauc.rand = {
+    gaussianDistribution(mean, stddev) {
+      let u = 0
+      let v = 0
 
-    while (u === 0) u = Math.random()
-    while (v === 0) v = Math.random()
+      while (u === 0) u = Math.random()
+      while (v === 0) v = Math.random()
 
-    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
-    return num * stddev + mean
-  },
+      let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+      return num * stddev + mean
+    },
 
-  range(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-  },
-}
+    range(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min
+    },
+  }
+})
